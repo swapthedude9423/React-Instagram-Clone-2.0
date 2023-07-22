b77ddba3e86b7325ec5ccd7133510384a098271f
Swapnil Meshram <swapnil.meshram@kratin.co.in>
2023-07-23 01:20:31 +0530

    first commit

---
+++ env.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "PORT": "5001",
+  "SESSION_SECRET_LETTER": "secret",
+  "MYSQL_HOST": "localhost",
+  "MYSQL_USER": "root",
+  "MYSQL_PASSWORD": "",
+  "MYSQL_DATABASE": "react-instagram-clone",
+  "MAIL": "",
+  "MAIL_PASSWORD": "",
+  "GOOGLE_GEOLOCATION_KEY": "google-geolocation-key",
+  "ADMIN_PASSWORD": "admin"
+}
