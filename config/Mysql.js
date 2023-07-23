c926d7ae144799cb76c9eda01841fc8ed5357341
Swapnil Meshram <swapnil.meshram@kratin.co.in>
2023-07-23 12:46:10 +0530

    create mysql pool

--- config/Mysql.js
+++ config/Mysql.js
@@ -4,23 +4,43 @@ const mysql = require('mysql'),
   { error } = require('handy-log'),
   { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env
 
-// CREATES A DB CONNECTION
-const db = mysql.createConnection({
+
+var db = mysql.createPool({
+  connectonLimit : 100,
   host: MYSQL_HOST,
   user: MYSQL_USER,
   password: MYSQL_PASSWORD,
   database: MYSQL_DATABASE,
   charset: 'utf8mb4',
-})
+});
 
-console.log("connection param : "+db)
+// CREATES A DB CONNECTION
+// const db = mysql.createConnection({
+//   host: MYSQL_HOST,
+//   user: MYSQL_USER,
+//   password: MYSQL_PASSWORD,
+//   database: MYSQL_DATABASE,
+//   charset: 'utf8mb4',
+// })
+
+//console.log("connection param : "+JSON.parse(db));
 // CONNECTS DB
-db.connect(err => {
-  console.log("connection init");
-  if (err) {
-    error("db connection error : "+err.message)
-  }
-  console.log("connection success");
-})
+// Attempt to catch disconnects 
+db.on('connection', function (connection) {
+  console.log('DB Connection established');
 
-module.exports = db
+  connection.on('error', function (err) {
+    console.error(new Date(), 'MySQL error', err.code);
+  });
+  connection.on('close', function (err) {
+    console.error(new Date(), 'MySQL close', err);
+  });
+// db.connect(err => {
+//   console.log("connection init");
+//   if (err) {
+//     error("db connection error : "+err.message)
+//   }
+//   console.log("connection success");
+ })
+
+module.exports = db;
